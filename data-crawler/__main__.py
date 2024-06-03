ecb44e3def9bbe5d8975175f3859d3045e42ac4f
Ricardo Mercado <ricardo.97.mc@gmail.com>
2024-06-03 15:59:35 +0100

    feat(data-crawler): init module

---
+++ data-crawler/__main__.py
@@ -0,0 +1,12 @@
+base_urls = {
+    'hl': 'https://www.hl.co.uk/shares/stock-market-summary/ftse-all-share?page='
+}
+
+
+def scrape(url):
+    return
+
+
+if __name__ == '__main__':
+    scrape(base_urls['hl'])
+    print('Done')
