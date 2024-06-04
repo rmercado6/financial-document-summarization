24cb09fc19791b9d6bd852f720ce045aec0f4878
Ricardo Mercado <ricardo.97.mc@gmail.com>
2024-06-04 14:45:39 +0100

    refactor(data_crawler): refactor hl_index_parser stock table parser and tests
    n

--- src/data_crawler/hl_index_parser.py
+++ src/data_crawler/hl_index_parser.py
@@ -5,8 +5,8 @@ def parse_stock_table(response: str) -> dict[str, str]:
     parser = etree.HTMLParser()
     selector = etree.fromstring(response, parser)
     data = {}
-    for share in selector.xpath("//table[@class='stockTable']/tbody/tr[@class='table-odd' or @class='table-alt']"):
-        e = share.xpath(".//td")[1]
+    for stock in selector.xpath("//table[@class='stockTable']/tbody/tr[@class='table-odd' or @class='table-alt']"):
+        e = stock.xpath(".//td")[1]
         name = e.xpath(".//a/text()")[0]
         href = e.xpath(".//a/@href")[0]
         data[name] = href
--- tests/hl_index_parser_tests.py
+++ tests/hl_index_parser_tests.py
@@ -6,12 +6,12 @@ from typing import Dict
 
 class MyTestCase(unittest.TestCase):
 
-    def test_parse_shares(self) -> None:
+    def test_parse_stock_table(self) -> None:
         with open('./tests/mocks/data_crawler/hl-index.mock.html', 'r') as f:
             mock_response = ''.join([line for line in f.readlines()])
-        shares: Dict[str, str] = parse_stock_table(mock_response)
-        self.assertEqual(dict, type(shares))
-        self.assertEqual(110, len(shares.keys()))
+        stocks: Dict[str, str] = parse_stock_table(mock_response)
+        self.assertEqual(dict, type(stocks))
+        self.assertEqual(110, len(stocks.keys()))
 
 
 if __name__ == '__main__':
