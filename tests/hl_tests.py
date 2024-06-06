1b451e1ec3dceeaad81cba1fd686d0e96defd138
Ricardo Mercado <ricardo.97.mc@gmail.com>
2024-06-06 10:53:46 +0100

    test(data-crawler): refactor tests and mocks file names

--- tests/hl_tests.py
+++ tests/hl_tests.py
@@ -5,7 +5,7 @@ from src.data_crawler.hl_parse import parse_stocks_table, parse_financial_statem
 class HlIndexParserTests(unittest.TestCase):
 
     def test_parse_stocks_table(self) -> None:
-        with open('./tests/mocks/data_crawler/hl-index.mock.html', 'r') as f:
+        with open('./tests/mocks/data_crawler/hl-stocks-table.mock.html', 'r') as f:
             mock_response = ''.join([line for line in f.readlines()])
         stocks: dict[str, str] = parse_stocks_table(mock_response)
         self.assertEqual(dict, type(stocks))
@@ -13,7 +13,7 @@ class HlIndexParserTests(unittest.TestCase):
         self.assertEqual('/shares/shares-search-results/0664097', stocks['4imprint Group plc'])
 
     def test_parse_financial_statements_and_reports(self) -> None:
-        with open('./tests/mocks/data_crawler/hl-index-financial-statements-abrdn.mock.html', 'r') as f:
+        with open('./tests/mocks/data_crawler/hl-financial-statements-abrdn.mock.html', 'r') as f:
             mock_response = ''.join([line for line in f.readlines()])
         fin_data: dict[str, str] = parse_financial_statements_and_reports(mock_response)
         self.assertEqual(dict, type(fin_data))
