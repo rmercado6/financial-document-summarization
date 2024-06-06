447e1dbb5030ea0e1f5f1ee4a243a95890d2f859
Ricardo Mercado <ricardo.97.mc@gmail.com>
2024-06-06 10:39:39 +0100

    feat(data-crawler): abstract request consmr and producer to fit general request for reusability

--- src/data_crawler/__main__.py
+++ src/data_crawler/__main__.py
@@ -8,6 +8,41 @@ from src.data_crawler.hl_index_parser import parse_stocks_table, parse_financial
 from constants import DATA_SRC_URLS, N_PAGES, HTTP_CLIENT_CONFIG
 
 
+async def request_producer(client: AsyncClient, queue: asyncio.Queue, requests: list[dict[str, any]]):
+    while len(requests) > 0:
+        r = requests.pop()
+        url = r['url'] + r['metadata']['url_append'] if 'url_append' in r['metadata'].keys() else r['url']
+        await queue.put({
+            'metadata': r['metadata'],
+            'request': client.request(method=r['method'], url=url),
+        })
+
+
+async def request_consumer(client: AsyncClient, queue: asyncio.Queue, consumer_function: Callable):
+    while True:
+        __queue_item = await queue.get()
+        __response = await __queue_item['request']
+        if __response.is_redirect:
+            m = __queue_item['metadata']
+            url = __response.headers['Location'] + m['url_append'] if 'url_append' in m.keys() else __response['url']
+            m.update({
+                'redirected': {
+                    'from': __response.request.url,
+                    'to': url
+                }
+            })
+            await queue.put({
+                'metadata': m,
+                'request': client.request(method=__response.request.method, url=url)
+            })
+        elif __response.is_success:
+            if iscoroutinefunction(consumer_function):
+                await consumer_function(__response.text)
+            else:
+                consumer_function(__response.text)
+        queue.task_done()
+
+
 async def scrape_hl_index_stocks_table(url: str, n_pages: int = 6) -> dict[str, str]:
     __stocks = {}
     try:
@@ -23,41 +58,24 @@ async def scrape_hl_index_stocks_table(url: str, n_pages: int = 6) -> dict[str, 
         raise e
 
 
-async def scrape_hl_index_stock_pages(_stocks: dict[str, str]):
-    __stocks_url_queue = list(_stocks.values()).copy()
+async def scrape_hl_index_stock_pages(stocks: dict[str, str]):
+    __stocks_url_queue = list(stocks.values()).copy()
     queue = asyncio.Queue()
     client = AsyncClient(**HTTP_CLIENT_CONFIG)
 
-    async def request_producer(_queue: asyncio.Queue):
-        while len(__stocks_url_queue) > 0:
-            await _queue.put({
-                'metadata': {
-                    'url_append': DATA_SRC_URLS['hl-financial-statement-and-reports']
-                },
-                'request': client.get(DATA_SRC_URLS['hl-base'] + __stocks_url_queue.pop() + DATA_SRC_URLS['hl-financial-statement-and-reports'])
-            })
+    requests = [
+        {
+            'metadata': {
+                'url_append': DATA_SRC_URLS['hl-financial-statement-and-reports']
+            },
+            'method': 'GET',
+            'url': DATA_SRC_URLS['hl-base'] + url
+        }
+        for url in stocks.values()
+    ]
 
-    async def request_consumer(_queue: asyncio.Queue, consumer_function: Callable):
-        while True:
-            __queue_item = await _queue.get()
-            __response = await __queue_item['request']
-            if __response.is_redirect:
-                print(__response)
-                await _queue.put({
-                    'request': client.get(
-                        __response.headers['Location'] + DATA_SRC_URLS['hl-financial-statement-and-reports']
-                    )
-                })
-            elif __response.is_success:
-                print(__response.url)
-                if iscoroutinefunction(consumer_function):
-                    await consumer_function(__response.text)
-                else:
-                    consumer_function(__response.text)
-            queue.task_done()
-
-    producers = [asyncio.create_task(request_producer(queue)) for _ in range(3)]
-    consumers = [asyncio.create_task(request_consumer(queue, parse_financial_statements_and_reports)) for _ in range(10)]
+    producers = [asyncio.create_task(request_producer(client, queue, requests)) for _ in range(3)]
+    consumers = [asyncio.create_task(request_consumer(client, queue, parse_financial_statements_and_reports)) for _ in range(10)]
 
     await asyncio.gather(*producers)
     print(' --- finish request producing ---')
