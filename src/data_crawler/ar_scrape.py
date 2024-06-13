579cd405bd0413cb4ab2c62db57cbf09c1b6728c
Ricardo Mercado <ricardo.97.mc@gmail.com>
2024-06-13 10:58:35 +0100

    test(data-crawler): add ar_scrape tests

--- src/data_crawler/ar_scrape.py
+++ src/data_crawler/ar_scrape.py
@@ -6,7 +6,7 @@ from src.data_crawler.constants import LOGGER_NAME
 logger = getLogger(LOGGER_NAME)
 
 
-def scrape_ar_index_stocks_table(url: str) -> dict[str, str]:
+def scrape_ar_stocks_table(url: str) -> dict[str, str]:
     """Function to scrape stocks table from AnnualReports Stocks Table page
 
     :param url: AnnualReports FTSE ALL-SHARE Stocks Table URL
@@ -14,7 +14,7 @@ def scrape_ar_index_stocks_table(url: str) -> dict[str, str]:
     return {}
 
 
-async def scrape_hl_index_stock_pages(stocks: dict[str, str]) -> None:
+async def scrape_ar_stock_pages(stocks: dict[str, str]) -> None:
     """Function to scrape stocks pages from AnnualReports
 
     :param stocks: dict containing stocks urls from stocks table
--- tests/data_crawler/ar_scrape_tests.py
+++ tests/data_crawler/ar_scrape_tests.py
@@ -1,9 +1,101 @@
 import unittest
+import logging
+import httpx
+import pypdf
+
+from io import BytesIO
+from unittest.mock import patch, MagicMock, AsyncMock
+
+from src.data_crawler.constants import LOGGING_CONFIG
+from src.data_crawler.ar_scrape import scrape_ar_stocks_table, scrape_ar_stock_pages
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+# Set up Logger
+logging.basicConfig(**LOGGING_CONFIG['testing'])
+logger = logging.getLogger('HL Tests')
+
+
+class ARScrapeStocksTableTest(unittest.TestCase):
+    """Test case for testing the scraping of the stocks table"""
+
+    def setUp(self):
+        logger.debug(f'{"-" * 20} Starting {self.__class__.__name__} case... {"-" * 20}')
+
+        # Load html response mocks from files
+        with open('./tests/mocks/data_crawler/hl-stocks-table.mock.html', 'r') as _:
+            self.stocks_table_response_mock = _.read()
+
+    @patch('httpx.get', new_callable=MagicMock)
+    def test_scrape_hl_index_stocks_table(self, async_client_mock: AsyncMock) -> None:
+        """Test the scraping of the stocks table"""
+        # Set up Mocks
+        async_client_mock.return_value = httpx.Response(
+            status_code=200,
+            content=self.stocks_table_response_mock
+        )
+
+        # Call method
+        r = scrape_ar_stocks_table('')
+
+        # Assert
+        async_client_mock.assert_awaited_once()
+        self.assertEqual(dict, type(r))
+        self.assertEqual(110, len(r.keys()))
+        self.assertTrue('abrdn' in r.keys())
+
+    def tearDown(self):
+        logger.debug(f'{"-" * 20} Ending {self.__class__.__name__} case... {"-" * 20}')
+
+
+class ARScrapeFirmsDetailPageTest(unittest.IsolatedAsyncioTestCase):
+    """Test the scraping of the financial statements page"""
+
+    def setUp(self):
+        logger.debug(f'{"-" * 20} Starting {self.__class__.__name__} case... {"-" * 20}')
+
+        # Load html response mocks from files
+        with open('./tests/mocks/data_crawler/hl-financial-statements-abrdn.mock.html', 'r') as _:
+            self.firms_detail_page_response_mock = _.read()
+
+        # Set mock request
+        self.request_mock = MagicMock(httpx.Request, method='GET', url='http://test.url')
+
+        # Set pdf response contents
+        writer = pypdf.PdfWriter()
+        writer.add_blank_page(100, 100)
+        byte_stream = BytesIO()
+        writer.write_stream(byte_stream)
+        byte_stream.seek(0)
+        self.pdf_response_mock = byte_stream.read()
+
+    @patch('httpx.AsyncClient.request', new_callable=AsyncMock)
+    async def test_scrape_hl_index_stock_pages(self, async_client_mock: AsyncMock) -> None:
+        """Test the scraping of the stocks' financial statements page"""
+        # Set Up Mocks
+        async_client_mock.side_effect = [
+            httpx.Response(200, content=self.firms_detail_page_response_mock, request=self.request_mock),
+            httpx.Response(200, content=self.pdf_response_mock, request=self.request_mock),
+            httpx.Response(200, content=self.pdf_response_mock, request=self.request_mock),
+            httpx.Response(200, content=self.pdf_response_mock, request=self.request_mock),
+            httpx.Response(200, content=self.pdf_response_mock, request=self.request_mock),
+            httpx.Response(200, content=self.pdf_response_mock, request=self.request_mock),
+            httpx.Response(200, content=self.pdf_response_mock, request=self.request_mock),
+            httpx.Response(200, content=self.pdf_response_mock, request=self.request_mock),
+            httpx.Response(200, content=self.pdf_response_mock, request=self.request_mock),
+            httpx.Response(200, content=self.pdf_response_mock, request=self.request_mock),
+            httpx.Response(200, content=self.pdf_response_mock, request=self.request_mock)
+        ]
+
+        # Call function
+        await scrape_ar_stock_pages({
+            'abrdn': '/Company/abrdn',
+        })
+
+        # Assert
+        self.assertEqual(11, async_client_mock.await_count)
+
+    def tearDown(self):
+        logger.debug(f'{"-" * 20} Ending {self.__class__.__name__} case... {"-" * 20}')
 
 
 if __name__ == '__main__':
