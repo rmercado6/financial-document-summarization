56850b457351185099fd7e0e39ab2a23125afe5d
Ricardo Mercado <ricardo.97.mc@gmail.com>
2024-06-13 11:34:39 +0100

    feat(data-crawler): implement ar stocks table scraping

--- src/data_crawler/ar_scrape.py
+++ src/data_crawler/ar_scrape.py
@@ -1,6 +1,9 @@
 from logging import getLogger
 
-from src.data_crawler.constants import LOGGER_NAME
+from httpx import Client
+
+from src.data_crawler.constants import LOGGER_NAME, HTTP_CLIENT_CONFIG
+from src.data_crawler.ar_parse import parse_stocks_table
 
 
 logger = getLogger(LOGGER_NAME)
@@ -11,7 +14,18 @@ def scrape_ar_stocks_table(url: str) -> dict[str, str]:
 
     :param url: AnnualReports FTSE ALL-SHARE Stocks Table URL
     """
-    return {}
+    logger.debug(f'Scraping AR stocks table from {url}')
+    __stocks: dict = {}
+    try:
+        client = Client(**HTTP_CLIENT_CONFIG)
+        response = client.get(url)
+        if response.status_code == 200:
+            __stocks = parse_stocks_table(response.text)
+        logger.debug('Finished scraping AR stocks table')
+        return __stocks
+    except Exception as e:
+        logger.error(f"Error scraping the AR stocks table from {url}:\n\t{e}")
+        raise e
 
 
 async def scrape_ar_stock_pages(stocks: dict[str, str]) -> None:
