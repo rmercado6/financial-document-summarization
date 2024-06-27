c30531dbb527f5d74d96f82164c20a37841a6db2
Ricardo Mercado <ricardo.97.mc@gmail.com>
2024-06-27 10:53:06 +0100

    feat(summarization): add refine module that implements the refine large document summarization method

---
+++ src/summarization/refine.py
@@ -0,0 +1,31 @@
+from langchain_core.prompts import PromptTemplate
+from langchain.chains.summarize import load_summarize_chain
+
+# Refine prompts
+question_prompt_template = """
+                  Please provide a summary of the following text.
+                  TEXT: {text}
+                  SUMMARY:
+                  """
+
+refine_prompt_template = """
+              Write a concise summary of the following text delimited by triple backquotes.
+              Return your response in bullet points which covers the key points of the text.
+              ```{text}```
+              BULLET POINT SUMMARY:
+              """
+
+
+def refine(model, input_documents: list):
+    question_prompt = PromptTemplate(template=question_prompt_template, input_variables=["text"])
+    refine_prompt = PromptTemplate(template=refine_prompt_template, input_variables=["text"])
+
+    refine_chain = load_summarize_chain(
+        model,
+        chain_type="refine",
+        question_prompt=question_prompt,
+        refine_prompt=refine_prompt,
+        return_intermediate_steps=False,
+    )
+
+    return refine_chain.invoke({"input_documents": input_documents})
