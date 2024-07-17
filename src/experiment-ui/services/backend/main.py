1e18c6b8a24cca827ed06016668728ebbc707cfc
Ricardo Mercado <ricardo.97.mc@gmail.com>
2024-07-17 10:41:59 +0100

    feat(experiment-ui): add document retrieval to api

--- src/experiment-ui/services/backend/main.py
+++ src/experiment-ui/services/backend/main.py
@@ -1,3 +1,5 @@
+import jsonlines
+
 from fastapi import FastAPI
 
 from fastapi.middleware.cors import CORSMiddleware
@@ -14,6 +16,24 @@ app.add_middleware(
 )
 
 
-@app.get("/")
+@app.get("/documents")
 def home():
-    return "Test, World!"
+    docs = []
+    with jsonlines.open('./out/data-crawler/data.jsonl') as reader:
+        for line in reader:
+            # line['preview'] = line.pop('doc')[:100]
+            line.pop('doc')
+            docs.append(line)
+    return docs
+
+
+@app.get("/documents/{title}/{ticker}/{document_type}/{year}")
+def document(title: str, ticker: str, document_type: str, year: str):
+    doc = None
+    with jsonlines.open('./out/data-crawler/data.jsonl') as reader:
+        for line in reader:
+            if line['title'] == title and line['ticker'] == ticker \
+                    and line['document_type'] == document_type and year == str(line['year']):
+                doc = line
+                break
+    return doc
