1850fce50429e62695610ecf95dcb07ce1e2cd16
Ricardo Mercado <ricardo.97.mc@gmail.com>
2024-07-17 11:33:58 +0100

    feat(experiment-ui/frontend): install tailwind css

--- src/experiment-ui/services/frontend/src/App.vue
+++ src/experiment-ui/services/frontend/src/App.vue
@@ -5,10 +5,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
+
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it!" class="text-red-500"/>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
