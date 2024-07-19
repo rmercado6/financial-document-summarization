db9c63385dc20cb460bcc19dfa5bd923200f52e9
Ricardo Mercado <ricardo.97.mc@gmail.com>
2024-07-20 00:20:43 +0100

    fix(experiment-ui/frontend): fix experiment datatable filter behaviour

--- src/experiment_ui/services/frontend/src/components/ExperimentDataTable.vue
+++ src/experiment_ui/services/frontend/src/components/ExperimentDataTable.vue
@@ -14,20 +14,27 @@ const experiments = experimentsStore.experiments;
 const loading = ref(true);
 const filters = ref({
     global: {value: null, matchMode: FilterMatchMode.CONTAINS},
-    document: {value: null, matchMode: FilterMatchMode.CONTAINS},
-    model: {value: null, matchMode: FilterMatchMode.CONTAINS},
-    pipeline: {value: null, matchMode: FilterMatchMode.CONTAINS},
+    'query.document.title': {value: null, matchMode: FilterMatchMode.CONTAINS},
+    'query.document.ticker': {value: null, matchMode: FilterMatchMode.CONTAINS},
+    'query.document.document_type': {value: null, matchMode: FilterMatchMode.CONTAINS},
+    'query.document.year': {value: null, matchMode: FilterMatchMode.CONTAINS},
+    'query.model': {value: null, matchMode: FilterMatchMode.CONTAINS},
+    'query.pipeline': {value: null, matchMode: FilterMatchMode.CONTAINS},
     time: {value: null, matchMode: FilterMatchMode.CONTAINS}
 });
 
 const selected_experiment = ref();
 
-function selectExperiment(){
-    router.push({name: 'experiment', params: selected_experiment.value})
+function selectExperiment() {
+    router.push({
+        name: 'experiment', params: {
+            uuid: selected_experiment.value.uuid
+        }
+    })
 }
 
 function getDocumentName(doc) {
-    return doc.title
+    return doc.title + ' [' + doc.ticker + '] '
 }
 
 onMounted(() => {
@@ -37,11 +44,12 @@ onMounted(() => {
 
 <template>
     <main>
-        <DataTable v-model:filters="filters"  :value="experiments" paginator :rows="9" filterDisplay="row"
+        <DataTable v-model:filters="filters" :value="experiments" paginator :rows="9" filterDisplay="row"
                    v-model:selection="selected_experiment" selectionMode="single" :metaKeySelection="false"
                    @rowSelect="selectExperiment"
                    :loading="loading"
-                   :globalFilterFields="['document', 'model', 'pipeline', 'time']">
+                   :globalFilterFields="[
+                       'query.document.title', 'query.document.ticker', 'query.document.document_type', 'query.document.year', 'query.model', 'query.pipeline', 'time']">
             <template #header>
                 <div class="flex justify-end">
                     <InputText v-model="filters['global'].value" placeholder="Search"/>
@@ -49,40 +57,39 @@ onMounted(() => {
             </template>
             <template #empty> No experiments found.</template>
             <template #loading> Loading experiments data. Please wait.</template>
-            <Column field="document" header="Document" style="min-width: 12rem">
+            <Column filterField="query.document.title" header="Document">
                 <template #body="{ data }">
-                    {{getDocumentName(data.query.document)}}
-                </template>
-                <template #filter="{ filterModel, filterCallback }">
-                    <InputText v-model="filterModel.value" type="text" @input="filterCallback()"
-                               placeholder="Search by document"/>
+                    {{data.query.document.title}}
                 </template>
             </Column>
-            <Column field="model" header="Model" style="min-width: 12rem">
+            <Column filterField="query.document.ticker" header="Ticker">
+                <template #body="{ data }">
+                    {{data.query.document.ticker}}
+                </template>
+            </Column>
+            <Column filterField="query.document.document_type" header="Type">
+                <template #body="{ data }">
+                    {{data.query.document.document_type.replace('_', ' ').replace(/\b\w/g, s => s.toUpperCase())}}
+                </template>
+            </Column>
+            <Column filterField="query.document.year" header="Year">
+                <template #body="{ data }">
+                    {{data.query.document.year}}
+                </template>
+            </Column>
+            <Column field="query.model" header="Model">
                 <template #body="{ data }">
                     {{data.query.model}}
                 </template>
-                <template #filter="{ filterModel, filterCallback }">
-                    <InputText v-model="filterModel.value" type="text" @input="filterCallback()"
-                               placeholder="Search by model"/>
-                </template>
             </Column>
-            <Column field="pipeline" header="Pipeline" style="min-width: 12rem">
+            <Column field="query.pipeline" header="Pipeline">
                 <template #body="{ data }">
                     {{data.query.pipeline}}
                 </template>
-                <template #filter="{ filterModel, filterCallback }">
-                    <InputText v-model="filterModel.value" type="text" @input="filterCallback()"
-                               placeholder="Search by pipeline"/>
-                </template>
             </Column>
-            <Column field="time" header="Time" style="min-width: 12rem">
+            <Column field="time" header="Time">
                 <template #body="{ data }">
                     {{data.time}}
-                </template>
-                <template #filter="{ filterModel, filterCallback }">
-                    <InputText v-model="filterModel.value" type="text" @input="filterCallback()"
-                               placeholder="Search by query time"/>
                 </template>
             </Column>
         </DataTable>
