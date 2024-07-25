2791582f5714ed569d468bec11e9453026a50d5a
Ricardo Mercado <ricardo.97.mc@gmail.com>
2024-07-25 18:25:38 +0100

    feat(experiment-ui/frontend): add text input for {task} and checkbox for filtering docs by similarity

--- src/experiment_ui/services/frontend/src/components/PromptForm.vue
+++ src/experiment_ui/services/frontend/src/components/PromptForm.vue
@@ -16,16 +16,25 @@ const store = useQueryLLMs();
 const model = ref('llama');
 const pipeline = ref('refine');
 const prompt_1 = ref(
-    'Please provide a summary of the following text.\n' +
-    'TEXT: {text}\n' +
-    'SUMMARY:'
+    "Context information is below.\n" +
+    "---------------------\n" +
+    "{text}\n" +
+    "---------------------\n" +
+    "Given the context information and no prior knowledge, {task}:"
 );
 const prompt_2 = ref(
-    'Write a concise summary of the following text delimited by triple backquotes.\n' +
-    'Return your response in bullet points which covers the key points of the text.\n' +
-    '```{text}```\n' +
-    'BULLET POINT SUMMARY:'
+    "The task is to {task}.\n" +
+    "We have provided an initial summary: {existing_answer}\n" +
+    "We have the opportunity to refine the existing summary\n" +
+    '(only if needed) with some more context below.\n' +
+    "------------\n" +
+    "{text}\n" +
+    "------------\n" +
+    "Given the new context, refine the original summary to better fit the task.\n" +
+    "You must provide a response, either the initial summary or refined summary."
 );
+const task = ref('make a summary');
+const similarity_filter = ref(false);
 
 function query_model() {
     store.query_model({
@@ -33,6 +42,8 @@ function query_model() {
         pipeline: pipeline.value,
         prompt_1: prompt_1.value,
         prompt_2: prompt_2.value,
+        task: task.value,
+        similarity_filter: similarity_filter.value,
         document: {
             title: props.title,
             year: props.year,
@@ -45,27 +56,27 @@ function query_model() {
 </script>
 
 <template>
-    <div class="flex flex-col h-full w-full">
-        <div class="flex gap-3 my-2 pb-2 pr-3 items-center border-b border-slate-100">
+    <div class="flex flex-col h-full w-full text-sm">
+        <div class="flex gap-3 mb-2 pb-2 pr-3 items-center border-b border-slate-100">
             <h3 class="text-xl px-1 grow font-semibold">
                 Query Model
             </h3>
         </div>
-        <div class="flex flex-col gap-3 overflow-y-auto rounded-md">
-            <div>
+        <div class="flex flex-col gap-2 overflow-y-auto rounded-md">
+            <div class="flex gap-2">
                 <h4>Model</h4>
-                <div class="flex gap-2">
+                <div class="flex flex-1 gap-2">
                     <span v-bind:class="model === 'gpt' ? 'select-pill active' : 'select-pill inactive'"
-                          @click="model = 'gpt'">ChatGPT 4 Turbo</span>
+                          @click="model = 'gpt'">gpt-4o-2024-05-13</span>
                     <span v-bind:class="model === 'mistral' ? 'select-pill active' : 'select-pill inactive'"
                           @click="model = 'mistral'">Mistral</span>
                     <span v-bind:class="model === 'llama' ? 'select-pill active' : 'select-pill inactive'"
                           @click="model = 'llama'">Llama</span>
                 </div>
             </div>
-            <div>
+            <div class="flex gap-2">
                 <h4>Pipeline</h4>
-                <div class="flex gap-2">
+                <div class="flex flex-1 gap-2">
                     <span v-bind:class="pipeline === 'refine' ? 'select-pill active' : 'select-pill inactive'"
                           @click="pipeline = 'refine'">Refine</span>
                     <span v-bind:class="pipeline === 'mapreduce' ? 'select-pill active' : 'select-pill inactive'"
@@ -73,12 +84,27 @@ function query_model() {
                 </div>
             </div>
             <div>
-                <h4 v-html="pipeline === 'refine' ? 'Question Prompt' : 'Map Prompt'"></h4>
+                <h4 v-html="pipeline === 'refine' ? 'Initial Prompt / Question Prompt' : 'Map Prompt'"></h4>
                 <textarea class="prompt-input" rows="7" v-model="prompt_1"></textarea>
             </div>
             <div>
                 <h4 v-html="pipeline === 'refine' ? 'Refine Prompt' : 'Combine Prompt'"></h4>
                 <textarea class="prompt-input" rows="7" v-model="prompt_2"></textarea>
+            </div>
+            <div>
+                <div class="flex gap-2">
+                    <h4>Task</h4>
+                    <p class="flex-1 italic text-slate-500 text-xs flex justify-end items-center">
+                        Replaces the variable `{task}` if provided on the prompts.
+                    </p>
+                </div>
+                <textarea class="prompt-input" rows="2" v-model="task"></textarea>
+            </div>
+            <div>
+                <div class="flex gap-2 px-2 justify-end">
+                    <input type="checkbox" id="checkbox" v-model="similarity_filter">
+                    <label for="checkbox">Filter relevant documents?</label>
+                </div>
             </div>
             <div class="flex justify-end">
                 <span class="btn" @click="query_model">
@@ -91,11 +117,11 @@ function query_model() {
 
 <style scoped>
 h4 {
-    @apply font-semibold mb-1;
+    @apply font-semibold mb-1 content-center;
 }
 
 span.select-pill {
-    @apply py-1 px-3 border rounded-md flex-1 text-center content-center font-mono text-sm;
+    @apply py-1 px-3 border rounded-md flex-1 text-center content-center font-mono text-xs;
 }
 
 span.select-pill.inactive {
@@ -108,7 +134,7 @@ span.select-pill.active {
 }
 
 textarea.prompt-input {
-    @apply overflow-x-clip overflow-y-scroll resize-none w-full border border-slate-200 rounded-md text-sm p-2;
+    @apply overflow-x-clip overflow-y-scroll resize-none w-full border border-slate-300 rounded-md text-sm p-2;
 }
 
 .btn {
